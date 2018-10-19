38066399c227e480b81b124da972c3e3e929b896
Anupam Dagar <dagaranupam@gmail.com>
2018-10-20 01:11:32 +0530

    add mysql support. Get column data from mysql table.

--- backend/urls.py
+++ backend/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^api/add/(?P<dbname>[-\w]+)/$',
         views.DB_detailsAPI.as_view(), name='DB_details'),
+    url(r'^api/mysql/(?P<column>[-\w]+)/(?P<dbname>[-\w]+)/(?P<tablename>[-\w]+)/$',
+        views.MysqlAPI.as_view(), name='Mysql'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -5,6 +5,28 @@ from rest_framework import permissions, status
 from .models import DB_details
 from .serializer import DB_detailsSerializer
 from django.http import HttpResponse, JsonResponse
+import MySQLdb
+from django.views.decorators.http import require_http_methods
+
+
+class dotdict(dict):
+   __getattr__ = dict.get
+   __setattr__ = dict.__setitem__
+   __delattr__ = dict.__delitem__
+
+
+def get_results(db_cursor, column):
+   desc = ""
+   results = []
+   iterator = 0
+   for d in db_cursor.description:
+       if d[0].lower() == column.lower():
+           desc = d[0]
+           break
+       iterator = iterator + 1
+    for res in db_cursor.fetchall():
+       results.append({desc:res[iterator]})
+   return results
 
 
 def home(request):
@@ -27,3 +49,14 @@ class DB_detailsAPI(APIView):
             return Response(response_data, status=status.HTTP_200_OK)
         except:
             return Response({"success": False, "message": "No details found"}, status=status.HTTP_400_BAD_REQUEST)
+
+class MysqlAPI(APIView):
+    def get(self, request, column, dbname, tablename):
+        db_data = DB_details.objects.get(name=dbname)
+         conn = MySQLdb.connect(host=db_data.address,
+                               user=db_data.username, passwd=db_data.password)
+        cursor = conn.cursor()
+        cursor.execute('use ' + db_data.name)
+        cursor.execute('select * from ' + tablename)
+         results = get_results(cursor, column)
+         return Response(results, status=status.HTTP_200_OK)
